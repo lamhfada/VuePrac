79bdad9cd672ba2de18249517db9070499e9ba9b
A014349 <A014349@ms.skh.org.tw>
2019-03-20 10:36:37 +0800

    sas lease enter the commit message for your changes. Lines starting owith '#' will be ignored, and an empty message aborts the commit.

---
+++ src/test/java/com/VuePrac/demo/CheckboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.VuePrac.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheckboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
